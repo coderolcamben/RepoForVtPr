60e561c268ce594244d0731b1b2c10596496c2b3
coderolcamben <69865549+coderolcamben@users.noreply.github.com>
2021-12-13 15:31:11 +0300

    Add files via upload

---
+++ VTYS_Proje/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace VTYS_Proje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtButce = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtGelir = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtOy = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.BtnListle = new System.Windows.Forms.Button();
+            this.BtnEkle = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TxtAra = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1228, 348);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(32, 427);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Film ID: ";
+            // 
+            // TxtID
+            // 
+            this.TxtID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtID.ForeColor = System.Drawing.Color.White;
+            this.TxtID.Location = new System.Drawing.Point(108, 424);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(100, 24);
+            this.TxtID.TabIndex = 2;
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtAd.ForeColor = System.Drawing.Color.White;
+            this.TxtAd.Location = new System.Drawing.Point(304, 424);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(100, 24);
+            this.TxtAd.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(222, 427);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Film Adı: ";
+            // 
+            // TxtButce
+            // 
+            this.TxtButce.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtButce.ForeColor = System.Drawing.Color.White;
+            this.TxtButce.Location = new System.Drawing.Point(518, 421);
+            this.TxtButce.Name = "TxtButce";
+            this.TxtButce.Size = new System.Drawing.Size(100, 24);
+            this.TxtButce.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(418, 424);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Film Butce: ";
+            // 
+            // TxtGelir
+            // 
+            this.TxtGelir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtGelir.ForeColor = System.Drawing.Color.White;
+            this.TxtGelir.Location = new System.Drawing.Point(721, 421);
+            this.TxtGelir.Name = "TxtGelir";
+            this.TxtGelir.Size = new System.Drawing.Size(100, 24);
+            this.TxtGelir.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(632, 424);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Film Gelir:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(835, 424);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Film Tarih:";
+            // 
+            // TxtOy
+            // 
+            this.TxtOy.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.TxtOy.ForeColor = System.Drawing.Color.White;
+            this.TxtOy.Location = new System.Drawing.Point(1118, 421);
+            this.TxtOy.Name = "TxtOy";
+            this.TxtOy.Size = new System.Drawing.Size(100, 24);
+            this.TxtOy.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(1040, 424);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Film Oy:";
+            // 
+            // BtnListle
+            // 
+            this.BtnListle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(183)))), ((int)(((byte)(184)))));
+            this.BtnListle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnListle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnListle.ForeColor = System.Drawing.Color.White;
+            this.BtnListle.Location = new System.Drawing.Point(166, 495);
+            this.BtnListle.Name = "BtnListle";
+            this.BtnListle.Size = new System.Drawing.Size(182, 38);
+            this.BtnListle.TabIndex = 13;
+            this.BtnListle.Text = "Listele";
+            this.BtnListle.UseVisualStyleBackColor = false;
+            this.BtnListle.Click += new System.EventHandler(this.BtnListle_Click);
+            // 
+            // BtnEkle
+            // 
+            this.BtnEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(183)))), ((int)(((byte)(184)))));
+            this.BtnEkle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnEkle.ForeColor = System.Drawing.Color.White;
+            this.BtnEkle.Location = new System.Drawing.Point(419, 495);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(182, 38);
+            this.BtnEkle.TabIndex = 14;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.UseVisualStyleBackColor = false;
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(183)))), ((int)(((byte)(184)))));
+            this.BtnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSil.ForeColor = System.Drawing.Color.White;
+            this.BtnSil.Location = new System.Drawing.Point(672, 495);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(182, 38);
+            this.BtnSil.TabIndex = 15;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = false;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(170)))), ((int)(((byte)(183)))), ((int)(((byte)(184)))));
+            this.BtnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGuncelle.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGuncelle.ForeColor = System.Drawing.Color.White;
+            this.BtnGuncelle.Location = new System.Drawing.Point(925, 495);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(182, 38);
+            this.BtnGuncelle.TabIndex = 16;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(918, 422);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(116, 24);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(432, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Ara : ";
+            // 
+            // TxtAra
+            // 
+            this.TxtAra.Location = new System.Drawing.Point(501, 17);
+            this.TxtAra.Name = "TxtAra";
+            this.TxtAra.Size = new System.Drawing.Size(353, 24);
+            this.TxtAra.TabIndex = 19;
+            this.TxtAra.TextChanged += new System.EventHandler(this.TxtAra_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(51)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1252, 573);
+            this.Controls.Add(this.TxtAra);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnEkle);
+            this.Controls.Add(this.BtnListle);
+            this.Controls.Add(this.TxtOy);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtGelir);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtButce);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtButce;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtGelir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtOy;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnListle;
+        private System.Windows.Forms.Button BtnEkle;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TxtAra;
+    }
+}
+
---
+++ VTYS_Proje/Form1.cs
@@ -0,0 +1,79 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VTYS_Proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NpgsqlConnection connection = new NpgsqlConnection("server=localHost; port=5432; Database=dbFilm; user ID=postgres; password=dondoberk1234");
+        private void BtnListle_Click(object sender, EventArgs e)
+        {
+            string query = "select * from film";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(query, connection);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            NpgsqlCommand command = new NpgsqlCommand("insert into film (film_id,film_ad,film_butce,film_gelir,film_tarih,film_oy) values (@p1,@p2,@p3,@p4,@p5,@p6)",connection);
+            command.Parameters.AddWithValue("@p1", int.Parse(TxtID.Text));
+            command.Parameters.AddWithValue("@p2",TxtAd.Text);
+            command.Parameters.AddWithValue("@p3", double.Parse(TxtButce.Text));
+            command.Parameters.AddWithValue("@p4", double.Parse(TxtGelir.Text));
+            command.Parameters.AddWithValue("@p5", dateTimePicker1.Value);
+            command.Parameters.AddWithValue("@p6", double.Parse(TxtOy.Text));
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            NpgsqlCommand command = new NpgsqlCommand("Delete from film where film_id=@p1", connection);
+            command.Parameters.AddWithValue("@p1", int.Parse(TxtID.Text));
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            NpgsqlCommand command = new NpgsqlCommand("update film set film_ad=@p1,film_butce=@p2,film_gelir=@p3,film_tarih=@p4,film_oy=@p5 where film_id=@p6", connection);
+            command.Parameters.AddWithValue("@p1", TxtAd.Text);
+            command.Parameters.AddWithValue("@p2", double.Parse(TxtButce.Text));
+            command.Parameters.AddWithValue("@p3", double.Parse(TxtGelir.Text));
+            command.Parameters.AddWithValue("@p4", dateTimePicker1.Value);
+            command.Parameters.AddWithValue("@p5", double.Parse(TxtOy.Text));
+            command.Parameters.AddWithValue("@p6", int.Parse(TxtID.Text));
+            command.ExecuteNonQuery();
+            connection.Close();
+
+        }
+
+        private void TxtAra_TextChanged(object sender, EventArgs e)
+        {
+            connection.Open();
+            DataTable tb1 = new DataTable();
+            NpgsqlDataAdapter ara = new NpgsqlDataAdapter("select * from film where film_ad like '%" + TxtAra.Text + "%'", connection);
+            ara.Fill(tb1);
+            dataGridView1.DataSource = tb1;
+            connection.Close();
+        }
+    }
+}
